0d16bc0a9d44f748a626e62b4835414719d1b246
rickie120243 <rickie120243@gmail.com>
2016-12-14 12:42:58 +0800

    add tibetan rules

---
+++ libs/index.js
@@ -0,0 +1,60 @@
+//r_super1 = /^([གདབམའ]?)(ར)([ྐྒྔྕྗྙྟྡྣྦྨྫ])([ྭྱྲླཱ]*?)([ིེཻོཽུ]*?)([གངདནབམའརལས]?)([དས]?)$/;
+//r_super2 = /^([གདབམའ]?)(ལ)([ྐྒྔྕྗྟྡྤྦྷ])([ྭྱྲླཱ]*?)([ིེཻོཽུ]*?)([གངདནབམའརལས]?)([དས]?)$/;
+//r_super3 = /^([གདབམའ]?)(ས)([ྐྒྔྙྟྡྣྤྦྨྩ])([ྭྱྲླཱ]*?)([ིེཻོཽུ]*?)([གངདནབམའརལས]?)([དས]?)$/;
+//r_only2 = /^([ཀཁགངཅཆཇཉཏཐདནཔཕབམཙཚཛཝཞཟའཡརལཤསཧཨ])([གངདནབམའརལས]?)$/;
+//r_only3up = /^([གདབམའ]?)([ཀཁགངཅཆཇཉཏཐདནཔཕབམཙཚཛཝཞཟའཡརལཤསཧཨ])([གངདནབམའརལས]?)([དས]?)$/;
+//r_vowel_sub = /^([གདབམའ]?)([ཀཁགངཅཆཇཉཏཐདནཔཕབམཙཚཛཝཞཟའཡརལཤསཧཨ])([ྭྱྲླཱ]*?)([ིེཻོཽུ]*?)([གངདནབམའརལས]?)([དས]?)$/;
+
+const pre = ['ག', 'ད', 'བ', 'མ', 'འ', ''];
+const sup = ['ར', 'ལ', 'ས'];
+const root1 = ['ྐ', 'ྒ', 'ྔ', 'ྕ', 'ྗ', 'ྙ', 'ྟ', 'ྡ', 'ྣ', 'ྦ', 'ྨ', 'ྫ'];
+const root2 = ['ྐ', 'ྒ', 'ྔ', 'ྕ', 'ྗ', 'ྟ', 'ྡ', 'ྤ', 'ྦ', 'ྷ'];
+const root3 = ['ྐ', 'ྒ', 'ྔ', 'ྙ', 'ྟ', 'ྡ', 'ྣ', 'ྤ', 'ྦ', 'ྨ', 'ྩ'];
+const root4 = ['ཀ', 'ཁ', 'ག', 'ང', 'ཅ', 'ཆ', 'ཇ', 'ཉ', 'ཏ', 'ཐ', 'ད', 'ན', 'པ', 'ཕ', 'བ', 'མ', 'ཙ', 'ཚ', 'ཛ', 'ཝ', 'ཞ', 'ཟ', 'འ', 'ཡ', 'ར', 'ལ', 'ཤ', 'ས', 'ཧ', 'ཨ'];
+const sub = ['ྱ', 'ླ', 'ཱ', 'ཱི', 'ུ', 'ཱུ', 'ྲྀ', 'ེ', 'ཻ', 'ོ', 'ཽ', ''];
+const vowel = ['ེ', 'ཻ', 'ོ', 'ཽ', ''];
+const post = ['ག', 'ང', 'ད', 'ན', 'བ', 'མ', 'འ', 'ར', 'ལ', 'ས', ''];
+const ppost = ['ད', 'ས', ''];
+const rulesArr = [tibetanRule1(), tibetanRule2(), tibetanRule3(), tibetanRule4(), tibetanRule5(), tibetanRule6()];
+
+Array.prototype.randomElement = function () {
+  return this[Math.floor(Math.random() * this.length)];
+}
+
+function tibetanRule1() {
+  const syl = pre.randomElement() + sup[0] + root1.randomElement() +
+    sub.randomElement() + post.randomElement() +
+    ppost.randomElement();
+  return syl;
+}
+
+function tibetanRule2() {
+  const syl = pre.randomElement() + sup[1] + root2.randomElement() +
+    sub.randomElement() + post.randomElement() +
+    ppost.randomElement();
+  return syl;
+}
+
+function tibetanRule3() {
+  const syl = pre.randomElement() + sup[2] + root3.randomElement() +
+    sub.randomElement() + post.randomElement() +
+    ppost.randomElement();
+  return syl;
+}
+
+function tibetanRule4() {
+  const syl = root4.randomElement() + post.randomElement();
+  return syl;
+}
+
+function tibetanRule5() {
+  const syl = pre.randomElement() + root4.randomElement() +
+    post.randomElement() + ppost.randomElement();
+  return syl;
+}
+
+function tibetanRule6() {
+  const syl = pre.randomElement() + root4.randomElement() + sub.randomElement() +
+    post.randomElement() + ppost.randomElement();
+  return syl;
+}
